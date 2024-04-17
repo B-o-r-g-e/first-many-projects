a247ab0981e110e3a911e5800117b661c280d119
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-04-17 15:04:20 +0100

    .

---
+++ first-many-projects/src/home/Home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import {CTA, Navbar, Brand} from "../Components/index.js";
+import {Header, Blog, Features,WhatGPT3, Footer, Possibility} from "../containers/index.js";
+
+
+
+const Home = () => {
+    return (
+        <div className={'App'}>
+            <div className="gradient__bg">
+                <Navbar/>
+                <Header/>
+            </div>
+            <Brand/>
+            <WhatGPT3/>
+            <Features/>
+            <Possibility/>
+            <CTA/>
+            <Blog/>
+            <Footer/>
+        </div>
+    )
+}
