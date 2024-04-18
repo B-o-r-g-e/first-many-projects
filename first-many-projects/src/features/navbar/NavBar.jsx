5222f173f27de0b5537ab78f47f2c030eb1c3469
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-04-18 15:15:13 +0100

    .

--- first-many-projects/src/features/navbar/NavBar.jsx
+++ first-many-projects/src/features/navbar/NavBar.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import './navbar.css'
-// import {Navbar} from "../../Components/index.js";
+import logo from "../../assets/logo.svg";
+import cart from "../../assets/cart.svg"
 
 const NavBar = () => (
-    <nav className="navbar navbar-default">
-        <div className="container">
-            <div className="navbar-header">
+    <nav className="navbar navbar-container">
+        <div className="gpt3__navbar-links__logo">
+            <img src={logo} alt="logo"/>
+        </div>
 
+        <ul className="navbar-nav">
+            <li className="nav-item">Home</li>
+            <li className="nav-item">Explore</li>
+            <li className="nav-item">Pricing</li>
+            <li className="nav-item">Blog</li>
+        </ul>
+
+        <div className="cart-signIn">
+            <div className="cart-container">
+                <img src={cart} alt="cart"/>
+            </div>
+            <div className="signIn-container">
+                <div className="signIn">Sign In</div>
             </div>
         </div>
     </nav>
