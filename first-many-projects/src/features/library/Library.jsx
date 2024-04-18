dc5880840e6c52b38966fe933a3affcea8fb4b4b
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-04-18 13:40:33 +0100

    .

--- first-many-projects/src/features/library/Library.jsx
+++ first-many-projects/src/features/library/Library.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import './library.css'
-import { Board, Tool, Library, TrialOffer, Working, Family, Simple, Footer, Brands, BottomBoard, DoingBoard, NewsArticle, Connect, DoingSecond, BrandBusiness, Tech, NavBar} from '../index.js'
+import { Board, Tool, TrialOffer, Working, Family, Simple, Footer, Brands, BottomBoard, DoingBoard, NewsArticle, Connect, DoingSecond, BrandBusiness, Tech, NavBar} from '../index.js'
+
 const Library = () => (
     <div className="library">
+        <NavBar />
         <Board />
+        <BottomBoard />
+        <Brands />
+        <Tool />
+        <BrandBusiness />
+        <Simple />
+        <DoingBoard />
+        <DoingSecond />
+        <Connect />
+        <Working />
+        <Family />
+        <NewsArticle />
+        <TrialOffer />
+        <Tech />
+        <Footer />
     </div>
 )
 
