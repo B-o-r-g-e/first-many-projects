a71ba1522473770b33a4b27e43c50bf4f1648fda
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-04-18 22:10:15 +0100

    .

--- first-many-projects/src/features/bottom-board/BottomBoard.jsx
+++ first-many-projects/src/features/bottom-board/BottomBoard.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import './bottom-board.css'
-// import {Navbar} from "../../Components/index.js";
+import logo from "../../assets/Logo1.png"
 
 const BottomBoard = () => (
     <div className="BottomBoard-container common">
         <div className="BottomBoard">
+            <div className="BottomBoard-top">
+                <div className="BottomBoard-top-left">
+                    <div className="BottomBoard-top-left-logo">
+                        <img src={logo} alt="logo"/>
+                    </div>
+                    <div className="BottomBoard-top-right-social">
+                        <img src="" alt=""/>
+                        <img src="" alt=""/>
+                        <img src="" alt=""/>
+                        <img src="" alt=""/>
+                        <img src="" alt=""/>
+                    </div>
+                </div>
 
+                <div className="BottomBoard-top-middle"></div>
+                <div className="BottomBoard-top-right"></div>
+            </div>
+
+            <div className="BottomBoard-bottom"></div>
         </div>
     </div>
 )
