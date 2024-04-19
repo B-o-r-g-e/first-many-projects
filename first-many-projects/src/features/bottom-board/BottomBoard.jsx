a9a6b2af8cecf4c2860fa70e2c3ce0235da9017d
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-04-19 10:18:33 +0100

    .

--- first-many-projects/src/features/bottom-board/BottomBoard.jsx
+++ first-many-projects/src/features/bottom-board/BottomBoard.jsx
@@ -6,6 +6,7 @@ import Pinterest from "../../assets/Pintrest.png"
 import instagram from "../../assets/Insta.png"
 import twitter from "../../assets/Twiter.png"
 import add from "../../assets/add.png"
+import love from "../../assets/love.png"
 
 const BottomBoard = () => (
     <div className="BottomBoard-container">
@@ -32,7 +33,16 @@ const BottomBoard = () => (
                     <div className="BottomBoard-top-right-today mid">Post</div>
                     <div className="BottomBoard-top-right-today mid">Blog</div>
                 </div>
-                <div className="BottomBoard-top-right"></div>
+
+                <div className="BottomBoard-top-right">
+                    <div className="BottomBoard-top-right-today mid prev">Preview</div>
+                    <div className="BottomBoard-top-right-today mid week">Week</div>
+                    <div className="BottomBoard-top-right-today mid month">Month</div>
+                    <div className="love">
+                        <img src={love} alt="love"/>
+
+                    </div>
+                </div>
             </div>
 
             <div className="BottomBoard-bottom"></div>
