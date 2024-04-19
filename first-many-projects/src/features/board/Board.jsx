795d1583130cea7d3b509e502bb9dea6aace8814
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-04-19 10:53:49 +0100

    .

--- first-many-projects/src/features/board/Board.jsx
+++ first-many-projects/src/features/board/Board.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import './board.css'
-import board1 from "../../assets/board1.png"
-// import {Navbar} from "../../Components/index.js";
 
 const Board = () => (
     <div className="Board common">
