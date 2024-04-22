4d34e079e7e7f353a01005a217c279f90d4407fd
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-04-22 21:24:55 +0100

    .

--- first-many-projects/src/features/Doing-board/DoingBoard.jsx
+++ first-many-projects/src/features/Doing-board/DoingBoard.jsx
@@ -4,8 +4,14 @@ import './doing-board.css'
 
 const DoingBoard = () => (
     <div className="doing-board">
-        Good
-        <p style={{color: 'white'}}>here is library</p>
+        <div className="doing-board-left">
+            <h1>Doing Everything
+                Yourself is Difficult</h1>
+            <p>Crush your social goals.Again and again</p>
+        </div>
+        <div className="doing-board-right">
+            <button>Contact Us</button>
+        </div>
     </div>
 )
 
