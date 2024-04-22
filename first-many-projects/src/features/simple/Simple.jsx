43fbf9c196c876094c623de4c47d7af5fbadc19d
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-04-22 19:31:23 +0100

    .

--- first-many-projects/src/features/simple/Simple.jsx
+++ first-many-projects/src/features/simple/Simple.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import './simple.css'
 import SimpleContents from "./simpleContents/SimpleContents.jsx";
 import calTime from '../../assets/caltime.svg'
+import calTime1 from '../../assets/caltime1.svg'
+import calTime2 from '../../assets/caltime2.svg'
+import calTime3 from '../../assets/caltime3.svg'
+import calTime4 from '../../assets/caltime4.svg'
+import calTime5 from '../../assets/caltime5.svg'
 
 const Simple = () => (
     <div className="simple-container">
@@ -11,6 +16,11 @@ const Simple = () => (
 
         <div className="simple-container-body">
             <SimpleContents categories={'Publish'} titles={'Save time by scheduling posts'} theIcon={calTime} />
+            <SimpleContents categories={'respond'} titles={'All your conversations in one place'} theIcon={calTime1} />
+            <SimpleContents categories={'Automate'} titles={'Truly automate your social media'} theIcon={calTime2} />
+            <SimpleContents categories={'monitor'} titles={'Listen to what they say about you'} theIcon={calTime3} />
+            <SimpleContents categories={'Analyze'} titles={'Detailed insights for your social media'} theIcon={calTime4} />
+            <SimpleContents categories={'Creative Content'} titles={'Artificial Intelligence trained Team'} theIcon={calTime5} />
         </div>
 
     </div>
