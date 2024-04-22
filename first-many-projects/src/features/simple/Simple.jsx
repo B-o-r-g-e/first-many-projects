ddd365aaca405b11535e6ae0e04f719daec16b66
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-04-22 09:20:11 +0100

    .

--- first-many-projects/src/features/simple/Simple.jsx
+++ first-many-projects/src/features/simple/Simple.jsx
@@ -26,6 +26,42 @@ const Simple = () => (
                     <button>Get Started</button>
                 </div>
             </div>
+            <div className="simple-container-body-contents">
+                <div className="simple-container-body-first">
+                    <p className="category">Publish</p>
+                    <h1 className="title">Save time
+                        by scheduling posts</h1>
+                    <p className="words">
+                        Building an enterprise doesn't need
+                        nightmare or cost your thousands.
+                        Felix is purpose built.</p>
+                </div>
+
+                <hr className="horizontal-line"/>
+
+                <div className="simple-container-body-second">
+                    <img src="" alt="icon"/>
+                    <button>Get Started</button>
+                </div>
+            </div>
+            <div className="simple-container-body-contents">
+                <div className="simple-container-body-first">
+                    <p className="category">Publish</p>
+                    <h1 className="title">Save time
+                        by scheduling posts</h1>
+                    <p className="words">
+                        Building an enterprise doesn't need
+                        nightmare or cost your thousands.
+                        Felix is purpose built.</p>
+                </div>
+
+                <hr className="horizontal-line"/>
+
+                <div className="simple-container-body-second">
+                    <img src="" alt="icon"/>
+                    <button>Get Started</button>
+                </div>
+            </div>
         </div>
 
     </div>
