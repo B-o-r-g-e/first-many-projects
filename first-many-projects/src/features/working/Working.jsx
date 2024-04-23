3b0d8aa0cc594cdcde01bf7f0d1a8ec15694d012
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-04-23 15:37:17 +0100

    .

--- first-many-projects/src/features/working/Working.jsx
+++ first-many-projects/src/features/working/Working.jsx
@@ -4,7 +4,13 @@ import './working.css'
 const Working = () => (
     <div className="working-container">
         <div className="working">
-            <div className="working-right"></div>
+            <div className="working-right">
+                <h1>Our Working Process - How
+                    We Work For Our Customers</h1>
+                <p>We currently support Facebook, Instagram,
+                    LinkedIn and Twitter. More to come.</p>
+                <button>Get Started</button>
+            </div>
 
             <div className="working-left"></div>
         </div>
