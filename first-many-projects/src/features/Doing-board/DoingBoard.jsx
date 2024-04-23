4413ba5cd7656c658c45f792bc7dc603fe82e560
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-04-23 10:31:58 +0100

    .

--- first-many-projects/src/features/Doing-board/DoingBoard.jsx
+++ first-many-projects/src/features/Doing-board/DoingBoard.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import './doing-board.css'
 
 const DoingBoard = () => (
-    <div className="doing-board">
-        <div className="doing-board-left">
-            <h1>Doing Everything
-                Yourself is Difficult</h1>
-            <p>Crush your social goals.Again and again</p>
-        </div>
-        <div className="doing-board-right">
-            <button>Contact Us</button>
+    <div className="doing-board-container">
+        <div className="doing-board">
+            <div className="doing-board-left">
+                <h1>Doing Everything <br/>
+                    Yourself is Difficult</h1>
+                <p>Crush your social goals.Again and again</p>
+            </div>
+            <div className="doing-board-right">
+                <button>Contact Us</button>
+            </div>
         </div>
     </div>
 )
