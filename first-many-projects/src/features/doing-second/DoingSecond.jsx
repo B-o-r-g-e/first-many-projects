0e5d19b30b2d1c4ac4fed587dd2c8310026da6b3
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-04-23 11:22:06 +0100

    .

--- first-many-projects/src/features/doing-second/DoingSecond.jsx
+++ first-many-projects/src/features/doing-second/DoingSecond.jsx
@@ -3,7 +3,7 @@ import './doing-second.css'
 // import {Navbar} from "../../Components/index.js";
 
 const DoingSecond = () => (
-    <div className="doing-second">
+    <div className="doing-second-container">
         Good
         <p style={{color: 'white'}}>here is library</p>
     </div>
