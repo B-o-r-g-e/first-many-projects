844734ec5defa49dc07930f36b09d96b1e5228cd
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-04-23 15:44:30 +0100

    .

--- first-many-projects/src/features/working/Working.jsx
+++ first-many-projects/src/features/working/Working.jsx
@@ -5,8 +5,8 @@ const Working = () => (
     <div className="working-container">
         <div className="working">
             <div className="working-right">
-                <h1>Our Working Process - How
-                    We Work For Our Customers</h1>
+                <h1>Our Working Process - How <br/>
+                    We Work For Our <br/> Customers</h1>
                 <p>We currently support Facebook, Instagram,
                     LinkedIn and Twitter. More to come.</p>
                 <button>Get Started</button>
