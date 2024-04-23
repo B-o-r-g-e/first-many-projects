d15c622a3de86d28cf251959124783cb9de5786a
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-04-23 14:23:44 +0100

    .

--- first-many-projects/src/features/connect/Connect.jsx
+++ first-many-projects/src/features/connect/Connect.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import './connect.css'
-// import {Navbar} from "../../Components/index.js";
+import socials from "../../assets/socials.png"
 
 const Connect = () => (
-    <div className="connect">
-        Good
-        <p style={{color: 'white'}}>here is library</p>
+    <div className="connect-container">
+        <div className="connect">
+            <div className="connect-left">
+                <h1>Connect all the social
+                    networks you love</h1>
+                <p>We currently support Facebook, Instagram, LinkedIn and Twitter. More to come. Felix is purpose built for ease of use and complete flexability.</p>
+                <button>Get Started</button>
+            </div>
+
+            <div className="connect-right">
+                <img src={socials} alt="socials"/>
+            </div>
+        </div>
     </div>
 )
 
