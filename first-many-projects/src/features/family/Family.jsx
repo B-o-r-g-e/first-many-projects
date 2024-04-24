e1b519d8f2dcb9fb1ebf89355c0e4bb6a6d6eef5
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-04-24 13:58:36 +0100

    .

--- first-many-projects/src/features/family/Family.jsx
+++ first-many-projects/src/features/family/Family.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import './family.css'
-// import {Navbar} from "../../Components/index.js";
+import chevronLeft from '../../assets/chevron-left.png'
+import chevronRight from '../../assets/chevron-right.png'
 
 const Family = () => (
     <div className="family-container">
        <div className="family">
            <div className="family-top">
-               <h1></h1>
+               <h1>Our Happy Family</h1>
+               <div className="modal">
+                   <button className="prev">
+                       <img src={chevronLeft} alt="previous"/>
+                   </button>
+                   <div className="mid">
+                       <div className="dp-container">
+                           <img src="" alt=""/>
+                           <img src="" alt=""/>
+                       </div>
+
+                       <div className="name-container">
+                           <div className="name"></div>
+                           <div className="position"></div>
+                       </div>
+
+                       <img src="" alt="" className="rating"/>
+
+                       <div className="details">
+
+                       </div>
+                   </div>
+                   <img src="" alt="" className="next"/>
+               </div>
            </div>
 
            <div className="family-bottom"></div>
