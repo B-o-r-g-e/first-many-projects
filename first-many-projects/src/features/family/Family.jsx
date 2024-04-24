76a5e6b4e5d73e4ac019987dc258d1f26d4f6f07
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-04-24 15:49:05 +0100

    .

--- first-many-projects/src/features/family/Family.jsx
+++ first-many-projects/src/features/family/Family.jsx
@@ -8,41 +8,43 @@ import star from '../../assets/Star.png'
 
 const Family = () => (
     <div className="family-container">
-       <div className="family">
-           <div className="family-top">
-               <h1>Our Happy Family</h1>
-               <div className="modal">
-                   <button className="modal-prev">
-                       <img src={chevronLeft} alt="modal-prev"/>
-                   </button>
-                   <div className="modal-mid">
-                       <div className="dp-container">
-                           <img className={'dp'} src={dp} alt=""/>
-                           <img className={'dp-icon'} src={dpIcon} alt=""/>
-                       </div>
+        <div className="family">
+            <div className="family-top">
+                <h1>Our Happy Family</h1>
+                <div className="modal">
+                    <button className="modal-prev">
+                        <img src={chevronLeft} alt="modal-prev"/>
+                    </button>
+                    <div className="modal-mid">
+                        <div className="dp-container">
+                            <img className={'dp'} src={dp} alt=""/>
+                            <img className={'dp-icon'} src={dpIcon} alt=""/>
+                        </div>
 
-                       <div className="name-container">
-                           <div className="name"> Mick G.,</div>
-                           <div className="position"> Head of Sales and Marketing</div>
-                       </div>
+                        <div className="name-container">
+                            <div className="name"> Mick G.,</div>
+                            <div className="position"> Head of Sales and Marketing</div>
+                        </div>
 
-                       <img src={star} alt="" className="rating"/>
+                        <img src={star} alt="" className="rating"/>
 
-                       <div className="details">
-                           Lorem Ipsum is simply dummy text
-                           of the printing and typesetting industry.
-                           Lorem Ipsum has been the industry's standard
-                           dummy text ever since the when an unknown printer
-                           took a galley of type and scrambled it to make a
-                           type specimen book.
-                       </div>
-                   </div>
-                   <img src={chevronRight} alt="" className="next"/>
-               </div>
-           </div>
+                        <div className="details">
+                            Lorem Ipsum is simply dummy text
+                            of the printing and typesetting industry.
+                            Lorem Ipsum has been the industry's standard
+                            dummy text ever since the when an unknown printer
+                            took a galley of type and scrambled it to make a
+                            type specimen book.
+                        </div>
+                    </div>
+                    <img src={chevronRight} alt="" className="next"/>
+                </div>
+            </div>
 
-           <div className="family-bottom"></div>
-       </div>
+            <div className="horizontal-line new-line"/>
+
+            <div className="family-bottom"></div>
+        </div>
     </div>
 )
 
