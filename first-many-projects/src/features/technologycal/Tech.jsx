6202282c4062013030a13e495f1d624f2bdfa856
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-04-25 20:42:28 +0100

    .

--- first-many-projects/src/features/technologycal/Tech.jsx
+++ first-many-projects/src/features/technologycal/Tech.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './tech.css'
+import logo from '../../assets/logo.svg'
 
 const Tech = () => (
     <div className="tech-container">
-        Good
-        <p style={{color: 'white'}}>here is library</p>
+        <div className="tech">
+            <div className="tech-logo-area">
+                <img className="tech-logo" src={logo} alt="Our Logo" />
+            </div>
+        </div>
     </div>
 )
 
