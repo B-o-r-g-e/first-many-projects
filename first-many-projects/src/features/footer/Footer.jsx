5693e5e3ce0c5b12080fb7f4f67d147dff2764e6
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-04-25 20:56:06 +0100

    .

--- first-many-projects/src/features/footer/Footer.jsx
+++ first-many-projects/src/features/footer/Footer.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import './footer.css'
-// import {Navbar} from "../../Components/index.js";
 
 const Footer = () => (
-    <div className="footer">
+    <div className="footer-container">
         Good
         <p style={{color: 'white'}}>here is library</p>
     </div>
--- first-many-projects/src/features/library/Library.jsx
+++ first-many-projects/src/features/library/Library.jsx
@@ -25,7 +25,7 @@ const Library = () => (
         <NewsArticle />
         <TrialOffer />
         <Tech />
-        {/*<Footer />*/}
+        <Footer />
     </div>
 )
 
