cfd7105950b69ceec5ef0db3fff5bee82055bdf3
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-04-25 01:46:40 +0100

    .

--- first-many-projects/src/features/trial-offer/TrialOffer.jsx
+++ first-many-projects/src/features/trial-offer/TrialOffer.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import './trial-offer.css'
-// import {Navbar} from "../../Components/index.js";
+import benefitIcon from '../../assets/benefitIcon.svg'
 
 const TrialOffer = () => (
-    <div className="trial-offer">
-        Good
-        <p style={{color: 'white'}}>here is library</p>
+    <div className="trial-offer-container">
+        <div className="trial-offer">
+            <div className="trial-offer-left">
+                <h1></h1>
+                <p></p>
+                <div className="input-area">
+                    <input type="text" placeholder={'Enter your Email'}/>
+                    <button>start</button>
+                </div>
+                <div className="benefits">
+                    <div className="each-benefits">
+                        <img src={benefitIcon} alt=""/>
+                        Free 7-day trial
+                    </div>
+                    <div className="each-benefits">
+                        <img src={benefitIcon} alt=""/>
+                        No credit card required
+                    </div>
+                    <div className="each-benefits">
+                        <img src={benefitIcon} alt=""/>
+                        Cancel anytime
+                    </div>
+                </div>
+            </div>
+
+            <div className="trial-offer-right"></div>
+        </div>
     </div>
 )
 
