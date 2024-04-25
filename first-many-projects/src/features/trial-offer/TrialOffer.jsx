f66a4d30869679bcb5373b39361c7dab54ccbdea
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-04-25 01:52:02 +0100

    .

--- first-many-projects/src/features/trial-offer/TrialOffer.jsx
+++ first-many-projects/src/features/trial-offer/TrialOffer.jsx
@@ -6,8 +6,8 @@ const TrialOffer = () => (
     <div className="trial-offer-container">
         <div className="trial-offer">
             <div className="trial-offer-left">
-                <h1></h1>
-                <p></p>
+                <h1>Start your 7-day free trial</h1>
+                <p>Felix can help bring your dream website to life with ease.</p>
                 <div className="input-area">
                     <input type="text" placeholder={'Enter your Email'}/>
                     <button>start</button>
