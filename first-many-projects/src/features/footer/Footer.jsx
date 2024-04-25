8cb18c9d2c599df92c6d48cd0218a97580e70f9a
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-04-25 22:04:43 +0100

    .

--- first-many-projects/src/features/footer/Footer.jsx
+++ first-many-projects/src/features/footer/Footer.jsx
@@ -7,51 +7,53 @@ const Footer = () => (
             <div className="footer-left">
                 <table>
                     <thead>
-                        <tr>
-                            <th>Resources</th>
-                            <th></th>
-                            <th>Utility Pages</th>
-                        </tr>
+                    <tr>
+                        <th>Resources</th>
+                        <th></th>
+                        <th>Utility Pages</th>
+                    </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Home</td>
-                            <td>Blog</td>
-                            <td>Start here</td>
-                        </tr>
-                        <tr>
-                            <td>About</td>
-                            <td>Blog Post</td>
-                            <td>Style guide</td>
-                        </tr>
-                        <tr>
-                            <td>Contact</td>
-                            <td>Careers</td>
-                            <td>Password protected</td>
-                        </tr>
-                        <tr>
-                            <td>Blog</td>
-                            <td>why choose us</td>
-                            <td>404 Not found</td>
-                        </tr>
-                        <tr>
-                            <td>Blog Post</td>
-                            <td>Case studies</td>
-                            <td>Licenses</td>
-                        </tr>
-                        <tr>
-                            <td>Pricing</td>
-                            <td></td>
-                            <td>Changelog</td>
-                        </tr>
-                        <tr>
-                            <td>Pricing Single</td>
-                            <td></td>
-                            <td></td>
-                        </tr>
+                    <tr>
+                        <td>Home</td>
+                        <td>Blog</td>
+                        <td>Start here</td>
+                    </tr>
+                    <tr>
+                        <td>About</td>
+                        <td>Blog Post</td>
+                        <td>Style guide</td>
+                    </tr>
+                    <tr>
+                        <td>Contact</td>
+                        <td>Careers</td>
+                        <td>Password protected</td>
+                    </tr>
+                    <tr>
+                        <td>Blog</td>
+                        <td>why choose us</td>
+                        <td>404 Not found</td>
+                    </tr>
+                    <tr>
+                        <td>Blog Post</td>
+                        <td>Case studies</td>
+                        <td>Licenses</td>
+                    </tr>
+                    <tr>
+                        <td>Pricing</td>
+                        <td></td>
+                        <td>Changelog</td>
+                    </tr>
+                    <tr>
+                        <td>Pricing Single</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
                     </tbody>
                 </table>
             </div>
+
+            <div className="vertical-line"></div>
 
             <div className="footer-right"></div>
         </div>
