e11cd3b65990db2e7c2b169d1793e3114c2f7b83
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-04-25 21:21:23 +0100

    .

--- first-many-projects/src/features/footer/Footer.jsx
+++ first-many-projects/src/features/footer/Footer.jsx
@@ -3,8 +3,58 @@ import './footer.css'
 
 const Footer = () => (
     <div className="footer-container">
-        Good
-        <p style={{color: 'white'}}>here is library</p>
+        <div className="footer">
+            <div className="footer-left">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Resources</th>
+                            <th></th>
+                            <th>Utility Pages</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Home</td>
+                            <td>Blog</td>
+                            <td>Start here</td>
+                        </tr>
+                        <tr>
+                            <td>About</td>
+                            <td>Blog Post</td>
+                            <td>Style guide</td>
+                        </tr>
+                        <tr>
+                            <td>Contact</td>
+                            <td>Careers</td>
+                            <td>Password protected</td>
+                        </tr>
+                        <tr>
+                            <td>Blog</td>
+                            <td>why choose us</td>
+                            <td>404 Not found</td>
+                        </tr>
+                        <tr>
+                            <td>Blog Post</td>
+                            <td>Case studies</td>
+                            <td>Licenses</td>
+                        </tr>
+                        <tr>
+                            <td>Pricing</td>
+                            <td></td>
+                            <td>Changelog</td>
+                        </tr>
+                        <tr>
+                            <td>Pricing Single</td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div className="footer-right"></div>
+        </div>
     </div>
 )
 
