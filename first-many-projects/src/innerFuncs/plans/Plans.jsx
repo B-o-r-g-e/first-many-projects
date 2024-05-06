98fa2ae1e0e8040ce369461fab054d86f6188679
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-05-06 16:24:51 +0100

    .

---
+++ first-many-projects/src/innerFuncs/plans/Plans.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ContactUs.css'
+
+const ContactUs = () => {
+    return (
+        <div className="contactUs">
+            This is the contactUs
+        </div>
+    )
+}
+
+export default ContactUs;
