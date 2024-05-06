5b6da27be8bcfa5c0875a22b6208ab2dd6b31381
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-05-06 16:40:09 +0100

    .

---
+++ first-many-projects/src/innerFuncs/caseStudy/CaseStudy.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './News.css'
+
+const News = () => {
+    return (
+        <div className="news">
+            This is the News
+        </div>
+    )
+}
+
+export default News;
