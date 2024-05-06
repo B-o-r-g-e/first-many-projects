cb1355996159a17a38d0b966d9d598db114736c4
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-05-06 16:43:01 +0100

    .

--- first-many-projects/src/features/technologycal/Tech.jsx
+++ first-many-projects/src/features/technologycal/Tech.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './tech.css'
 import logo from '../../assets/logo.svg'
+import {Link} from "react-router-dom";
 
 const Tech = () => (
     <div className="tech-container">
         <div className="tech">
             <img src={logo} alt="Our Logo" />
-            <button>See our Case Studies</button>
+            <Link to='/CaseStudy'>
+                <button>See our Case Studies</button>
+            </Link>
         </div>
     </div>
 )
