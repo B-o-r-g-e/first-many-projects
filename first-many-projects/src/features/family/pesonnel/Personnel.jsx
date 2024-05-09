2acb7ec70e772bf5ec84a8a4442c39694791fad6
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-05-09 15:17:59 +0100

    .

---
+++ first-many-projects/src/features/family/pesonnel/Personnel.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import dpIcon from "../../../assets/dpIcon.png";
+import star from "../../../assets/Star.png";
+
+const Personnel = ({ image, name, position }) => {
+    return (
+        <div className={`slide modal-mid`}>
+            <div className="dp-container">
+                <img className={'dp'} src={image} alt=""/>
+                <img className={'dp-icon'} src={dpIcon} alt=""/>
+            </div>
+
+            <div className="name-container">
+                <div className="name"> {name},</div>
+                <div className="position"> {position}/div>
+                </div>
+
+                <img src={star} alt="" className="rating"/>
+
+                <div className="details">
+                    Lorem Ipsum is simply dummy text
+                    of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard
+                    dummy text ever since the when an unknown printer
+                    took a galley of type and scrambled it to make a
+                    type specimen book.
+                </div>
+            </div>
+        </div>
+    )
+}
