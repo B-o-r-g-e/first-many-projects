971b40926c5a0a9b4392ba94e023a2c9f9c6d225
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-05-10 18:57:26 +0100

    .

--- first-many-projects/src/innerFuncs/plans/plansDifference/plansDifference.jsx
+++ first-many-projects/src/innerFuncs/plans/plansDifference/plansDifference.jsx
@@ -49,58 +49,75 @@ const PlansDifference= () => {
                     </div>
                     <div className="diff-bottom">
                         <div className="free">Free for 3 months</div>
-                        <div className="diff-bottom-top">
-                            <div className="premium-package">
-                                <img src="" alt=""/>
-                                Premium
+                        <div className="within">
+                            <div className="diff-bottom-top">
+                                <div className="premium-package">
+                                    <img src={logo} alt="logo"/>
+                                    Premium
+                                </div>
+                                <h1 className={'individual'}>Individual</h1>
+                                <div className="price-container">
+                                    <p className='for'>Free for 3 months</p>
+                                    <p className='price'>$10.99 / month after</p>
+                                </div>
                             </div>
-                            <h1>Individual</h1>
-                            <div className="price-container">
-                                <h1>Free for 3 months</h1>
-                                <p>$10.99 / month after</p>
-                            </div>
-                        </div>
 
-                        <div className="horizontal-line new-line"/>
+                            <div className="horizontal-line new-line"/>
 
-                        <div className="diff-bottom-bottom">
-                            <div className="criteria"></div>
-                            <div className="activate-container">
-                                <button>Free for 3 months</button>
-                                <p>Individual plan only. $10.99/month after. Terms and conditions
-                                    apply. Open only to users who haven't already tried Premium.
-                                    Offer ends May 21, 2024.04
-                                </p>
+                            <div className="diff-bottom-bottom">
+                                <div className="criteria">
+                                    <ul>
+                                        <li>1 Premium account</li>
+                                        <li>Cancel anytime</li>
+                                        <li>15 hours/month of listening time from our audiobooks subscriber catalog</li>
+                                    </ul>
+                                </div>
+                                <div className="activate-container">
+                                    <button>Free for 3 months</button>
+                                    <p>Individual plan only. $10.99/month after. Terms and conditions
+                                        apply. Open only to users who haven't already tried Premium.
+                                        Offer ends May 21, 2024.04
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="diff-bottom">
                         <div className="free">Free for 3 months</div>
-                        <div className="diff-bottom-top">
-                            <div className="premium-package">
-                                <img src="" alt=""/>
-                                Premium
+                        <div className="within">
+                            <div className="diff-bottom-top">
+                                <div className="premium-package">
+                                    <img src={logo} alt="logo"/>
+                                    Premium
+                                </div>
+                                <h1 className={'individual'}>Individual</h1>
+                                <div className="price-container">
+                                    <p className='for'>Free for 3 months</p>
+                                    <p className='price'>$10.99 / month after</p>
+                                </div>
                             </div>
-                            <h1>Individual</h1>
-                            <div className="price-container">
-                                <h1>Free for 3 months</h1>
-                                <p>$10.99 / month after</p>
-                            </div>
-                        </div>
 
-                        <div className="horizontal-line new-line"/>
+                            <div className="horizontal-line new-line"/>
 
-                        <div className="diff-bottom-bottom">
-                            <div className="criteria"></div>
-                            <div className="activate-container">
-                                <button>Free for 3 months</button>
-                                <p>Individual plan only. $10.99/month after. Terms and conditions
-                                    apply. Open only to users who haven't already tried Premium.
-                                    Offer ends May 21, 2024.04
-                                </p>
+                            <div className="diff-bottom-bottom">
+                                <div className="criteria">
+                                    <ul>
+                                        <li>1 Premium account</li>
+                                        <li>Cancel anytime</li>
+                                        <li>15 hours/month of listening time from our audiobooks subscriber catalog</li>
+                                    </ul>
+                                </div>
+                                <div className="activate-container">
+                                    <button>Free for 3 months</button>
+                                    <p>Individual plan only. $10.99/month after. Terms and conditions
+                                        apply. Open only to users who haven't already tried Premium.
+                                        Offer ends May 21, 2024.04
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    \
                 </div>
             </div>
         </div>
