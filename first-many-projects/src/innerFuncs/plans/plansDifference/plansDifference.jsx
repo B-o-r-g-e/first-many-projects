9cc10f009c0c11b9bc5c45f406b3619d8de83a69
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-05-10 17:22:39 +0100

    .

--- first-many-projects/src/innerFuncs/plans/plansDifference/plansDifference.jsx
+++ first-many-projects/src/innerFuncs/plans/plansDifference/plansDifference.jsx
@@ -13,28 +13,30 @@ const PlansDifference= () => {
                 <div className="diff-bottom-container">
                     <div className="diff-bottom">
                         <div className="free">Free for 3 months</div>
-                        <div className="diff-bottom-top">
-                            <div className="premium-package">
-                                <img src="" alt=""/>
-                                Premium
+                        <div className="within">
+                            <div className="diff-bottom-top">
+                                <div className="premium-package">
+                                    <img src="" alt=""/>
+                                    Premium
+                                </div>
+                                <h1>Individual</h1>
+                                <div className="price-container">
+                                    <h1>Free for 3 months</h1>
+                                    <p>$10.99 / month after</p>
+                                </div>
                             </div>
-                            <h1>Individual</h1>
-                            <div className="price-container">
-                                <h1>Free for 3 months</h1>
-                                <p>$10.99 / month after</p>
-                            </div>
-                        </div>
 
-                        <div className="horizontal-line new-line"/>
+                            <div className="horizontal-line new-line"/>
 
-                        <div className="diff-bottom-bottom">
-                            <div className="criteria"></div>
-                            <div className="activate-container">
-                                <button>Free for 3 months</button>
-                                <p>Individual plan only. $10.99/month after. Terms and conditions
-                                    apply. Open only to users who haven't already tried Premium.
-                                    Offer ends May 21, 2024.04
-                                </p>
+                            <div className="diff-bottom-bottom">
+                                <div className="criteria"></div>
+                                <div className="activate-container">
+                                    <button>Free for 3 months</button>
+                                    <p>Individual plan only. $10.99/month after. Terms and conditions
+                                        apply. Open only to users who haven't already tried Premium.
+                                        Offer ends May 21, 2024.04
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
