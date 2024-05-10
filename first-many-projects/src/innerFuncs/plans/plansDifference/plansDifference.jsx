f15f5181de2df9d4872a6191149b91b78e00e3b6
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-05-10 20:03:34 +0100

    .

--- first-many-projects/src/innerFuncs/plans/plansDifference/plansDifference.jsx
+++ first-many-projects/src/innerFuncs/plans/plansDifference/plansDifference.jsx
@@ -14,8 +14,8 @@ const PlansDifference= () => {
                 <div className="diff-bottom-container">
                     <DiffBottom price={10.99} type={'Individual'} day={3} days={'Try free for 3 months'} />
                     <DiffBottom change={'student'} price={5.99} type={'Student'} day={1} days={'Try free for 1 months'} />
-                    <DiffBottom price={14.99} type={'Duo'} day={0} days={'Get Premium Duo'} />
-                    <DiffBottom price={16.99} type={'Family'} days={'Get Premium Family'} day={0} />
+                    <DiffBottom change={'duo'} price={14.99} type={'Duo'} day={0} days={'Get Premium Duo'} />
+                    <DiffBottom change={'fam'} price={16.99} type={'Family'} days={'Get Premium Family'} day={0} />
                 </div>
             </div>
         </div>
