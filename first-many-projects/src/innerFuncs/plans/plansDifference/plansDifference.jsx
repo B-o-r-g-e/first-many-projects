8b6648167d77dcfef9f774992fa36e68bb9f8155
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-05-10 16:20:15 +0100

    .

--- first-many-projects/src/innerFuncs/plans/plansDifference/plansDifference.jsx
+++ first-many-projects/src/innerFuncs/plans/plansDifference/plansDifference.jsx
@@ -4,7 +4,14 @@ import './plansDifference.css';
 const PlansDifference= () => {
     return (
         <div className="plans-difference">
+            <div className="diff-top">
+                <h1>Experience the difference</h1>
+                <p>Go Premium and enjoy full control. Cancel anytime.</p>
+            </div>
 
+            <div className="diff-bottom">
+
+            </div>
         </div>
     )
 }
