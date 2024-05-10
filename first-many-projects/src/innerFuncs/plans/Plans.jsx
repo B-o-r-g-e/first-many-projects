3f150dd7a460c78e7d508e8cc41acdfea8890149
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-05-10 16:08:03 +0100

    .

--- first-many-projects/src/innerFuncs/plans/Plans.jsx
+++ first-many-projects/src/innerFuncs/plans/Plans.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Plans.css'
+import {Navbar} from "../../Components/index.js";
+// import {Header} from "../../containers/index.js";
+import PlansHeader from "./plansHeader/PlansHeader.jsx";
 
 const Plans = () => {
     return (
         <div className="plans">
-            This is the Plans
+            <div className="gradient__bg">
+                <Navbar/>
+                <PlansHeader/>
+            </div>
+            <div className="plans-difference-container">
+
+            </div>
         </div>
     )
 }
