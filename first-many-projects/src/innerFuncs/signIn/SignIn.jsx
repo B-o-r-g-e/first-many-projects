b597d0d3fde14cdb67f6192a610396e86813036c
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-05-15 13:58:00 +0100

    .

--- first-many-projects/src/innerFuncs/signIn/SignIn.jsx
+++ first-many-projects/src/innerFuncs/signIn/SignIn.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './SignIn.css'
 import {auth, provider} from "./googleSignIn/config.jsx";
-import {signinWithPopup} from "firebase/auth"
+import {signInWithPopup} from "firebase/auth"
+import Home from "../../home/Home.jsx";
 
 const SignIn = () => {
-    const handleClick = () => [
-        signinWithPopup(auth, provider)
-    ]
+    const [values, setValues] = React.useState('')
+    const handleClick = () => {
+        signInWithPopup(auth, provider).then((data) => {
+            setValues(data.user.email)
+            localStorage.setItem("email", data.user.email)
+        })
+    }
+
+    useEffect(() => {
+        setValues(localStorage.getItem("email"))
+    }, []);
 
     return (
         <div className="signIn">
-            <button>Sign-in with Google</button>
+            {values? <Home /> :
+                <button onClick={handleClick}>Sign-in with Google</button>
+            }
         </div>
+
     )
 }
 
