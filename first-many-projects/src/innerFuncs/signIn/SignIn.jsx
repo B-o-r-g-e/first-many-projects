cba1a359a137a545bd10a607048ed3d4268ae6fc
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-05-15 13:20:13 +0100

    .

--- first-many-projects/src/innerFuncs/signIn/SignIn.jsx
+++ first-many-projects/src/innerFuncs/signIn/SignIn.jsx
@@ -4,9 +4,13 @@ import {auth, provider} from "./googleSignIn/config.jsx";
 import {signinWithPopup} from "firebase/auth"
 
 const SignIn = () => {
+    const handleClick = () => [
+        signinWithPopup(auth, provider)
+    ]
+
     return (
         <div className="signIn">
-            This is the SignIn
+            <button>Sign-in with Google</button>
         </div>
     )
 }
