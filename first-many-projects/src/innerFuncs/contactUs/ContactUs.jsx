86cf9881903e12cb0114a7488ece07676135dd07
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-06-10 13:00:08 +0100

    .

--- first-many-projects/src/innerFuncs/contactUs/ContactUs.jsx
+++ first-many-projects/src/innerFuncs/contactUs/ContactUs.jsx
@@ -9,6 +9,11 @@ const ContactUs = () => {
                 <div className="contactUs-nav">
                     <Navbar />
                 </div>
+                <div className="contactUs-header">
+                    <h1>Let's Connect and Navigate Your Tech Goals</h1>
+                    <p>We are here to listen, advice, and help you achieve
+                        your tech dreams. Schedule your call today</p>
+                </div>
             </div>
         </div>
     )
