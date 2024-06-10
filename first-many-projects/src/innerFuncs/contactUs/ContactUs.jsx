1b378c337040bf74281bf739838837fc70ac280e
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-06-10 12:56:45 +0100

    .

--- first-many-projects/src/innerFuncs/contactUs/ContactUs.jsx
+++ first-many-projects/src/innerFuncs/contactUs/ContactUs.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './ContactUs.css'
+import {Navbar} from "../../Components/index.js";
 
 const ContactUs = () => {
     return (
-        <div className="contactUs">
-            This is the contactUs
+        <div className="contactUs-container">
+            <div className="contactUs">
+                <div className="contactUs-nav">
+                    <Navbar />
+                </div>
+            </div>
         </div>
     )
 }
