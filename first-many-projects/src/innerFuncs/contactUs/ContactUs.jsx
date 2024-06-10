ee7a388b188987d8b095ed4c9fe0cef2085558b1
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-06-10 14:34:46 +0100

    .

--- first-many-projects/src/innerFuncs/contactUs/ContactUs.jsx
+++ first-many-projects/src/innerFuncs/contactUs/ContactUs.jsx
@@ -14,21 +14,24 @@ const ContactUs = () => {
                     <p>We are here to listen, advice, and help you achieve
                         your tech dreams. Schedule your call today</p>
                 </div>
-                <div className="contactUs-form">
-                    <div className="contactUs-form-each">
-                        <p>Name</p>
-                        <input type="email" placeholder='Hello...' />
-                    </div>
+                <div className="contactUs-form-container">
+                    <div className="contactUs-form">
+                        <div className="contactUs-form-each">
+                            <p>Name</p>
+                            <input type="email" placeholder='Hello...'/>
+                        </div>
 
-                    <div className="contactUs-form-each">
-                        <p>Email</p>
-                        <input type='text' placeholder='Where should I reply'/>
-                    </div>
+                        <div className="contactUs-form-each">
+                            <p>Email</p>
+                            <input type='text' placeholder='Where should I reply'/>
+                        </div>
 
-                    <div className="contactUs-form-each message">
-                        <p>Message</p>
-                        <input type="text" placeholder='I want a help for...'/>
+                        <div className="contactUs-form-each message">
+                            <p>Message</p>
+                            <input type="text" placeholder='I want a help for...'/>
+                        </div>
                     </div>
+                    <button>Submit</button>
                 </div>
             </div>
         </div>
